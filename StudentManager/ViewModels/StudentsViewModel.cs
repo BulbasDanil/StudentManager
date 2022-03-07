7fadf5d411c0ee61eae0b5fb7c99a76d13e95ab0
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2022-03-07 02:18:21 -0500

    feat: started working over StudentViewModel

---
+++ StudentManager/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,85 @@
+﻿using StudentManager.Commands;
+using StudentManager.EDM;
+using StudentManager.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace StudentManager.ViewModels
+{
+    public class StudentsViewModel : INotifyPropertyChanged
+    {
+        private DataManager _dataManager;
+        public List<Student> Students { get; set; }
+
+        private Student _selectedStudent { get; set; }
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged("SelectedStudent");
+            }
+        }
+
+        public StudentsViewModel()
+        {
+            _dataManager = new DataManager();
+            Students = _dataManager.Students.ToList();
+        }
+
+        // CRUD Commands
+        private RelayCommand _addStudentCommand;
+        public RelayCommand AddStudentCommand
+        {
+            get
+            {
+                return _addStudentCommand ??
+                (_addStudentCommand = new RelayCommand(obj =>
+              {
+                  Student student = new Student()
+                  {
+                      Name = "",
+                      Surname = "",
+                      BirthDate = new System.DateTime(),
+                      EnrollmentYear = new System.DateTime(),
+                      Phone = "",
+                      Email = "",
+                      Adress = "",
+                      GroupId = 0,
+                      IsElder = false,
+
+                  };
+                  Students.Add(student);
+                  _dataManager.SaveChanges();
+                  SelectedStudent = student;
+              }));
+            }
+        }
+
+        private RelayCommand _saveStudentCommand;
+        public RelayCommand SaveStudentCommand
+        {
+            get
+            {
+                return _saveStudentCommand ??
+                    (_saveStudentCommand = new RelayCommand(obj =>
+                    {
+
+                    }));
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
